82b38d9f1f9a5ba68201bc4ae52ace689c15ff90
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-25 18:47:58 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 
-Vue.config.productionTip = false
+//ボタンの色をグローバルに設定
+const options = {
+  confirmButtonColor: "#41b882",
+  cancelButtonColor: "#ff7674",
+};
+
+Vue.use(VueSweetalert2, options);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
